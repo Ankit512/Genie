704f5a413dad1c085d968960bdbdca8962aabc20
Ankit <ankit@mac.home>
2025-07-17 22:57:48 +0100

    fix: Remove unused React import from PasswordStrengthIndicator

--- client/src/components/ui/PasswordStrengthIndicator.tsx
+++ client/src/components/ui/PasswordStrengthIndicator.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { validatePasswordStrength, getPasswordStrengthColor, getPasswordStrengthBgColor } from '../../lib/passwordValidation';
 
 interface PasswordStrengthIndicatorProps {
